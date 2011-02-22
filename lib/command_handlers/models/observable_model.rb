93d05160483407427c98c01dd3f6975e6476e07b
Andy Maleh <andy@obtiva.com>
2011-02-22 04:33:40 -0600

    Removed references to Eclipse license

--- lib/command_handlers/models/observable_model.rb
+++ lib/command_handlers/models/observable_model.rb
@@ -1,14 +1,3 @@
-################################################################################
-# Copyright (c) 2008 Annas Al Maleh.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
-#
-# Contributors:
-#    Annas Al Maleh - initial API and implementation
-################################################################################ 
-
 module ObservableModel
   
   def add_observer(property_name, observer)
