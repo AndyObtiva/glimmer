a879c4c88e39c4a3af7f23f65a363238a1e1bfb7
Andy Maleh <andy@obtiva.com>
2011-03-18 14:49:07 -0600

    Hello list samples

---
+++ samples/hello_list_multi_selection.rb
@@ -0,0 +1,46 @@
+require File.dirname(__FILE__) + "/../lib/glimmer"
+
+class Person 
+  attr_accessor :provinces, :provinces_options
+  
+  def initialize
+    self.provinces_options=[
+      "", 
+      "Quebec", 
+      "Ontario", 
+      "Manitoba", 
+      "Saskatchewan", 
+      "Alberta", 
+      "British Columbia", 
+      "Nova Skotia", 
+      "Newfoundland"
+    ]
+    self.provinces = ["Quebec", "Manitoba", "Alberta"]
+  end
+  
+  def reset_provinces
+    self.provinces = ["Quebec", "Manitoba", "Alberta"]
+  end  
+end
+
+class HelloListMultiSelection
+  include Glimmer
+  def launch
+    person = Person.new
+    shell {
+      composite {
+        list(:multi) {
+          selection bind(person, :provinces)
+        }
+        button {
+          text "Reset"
+          on_widget_selected do
+            person.reset_provinces
+          end
+        }
+      }
+    }.open
+  end
+end
+
+HelloListMultiSelection.new.launch
---
+++ samples/hello_list_single_selection.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + "/../lib/glimmer"
+
+class Person 
+  attr_accessor :country, :country_options
+  
+  def initialize
+    self.country_options=["", "Canada", "US", "Mexico"]
+    self.country = "Canada"
+  end
+  
+  def reset_country
+    self.country = "Canada"
+  end  
+end
+
+class HelloListSingleSelection
+  include Glimmer
+  def launch
+    person = Person.new
+    shell {
+      composite {
+        list {
+          selection bind(person, :country)
+        }
+        button {
+          text "Reset"
+          on_widget_selected do
+            person.reset_country
+          end
+        }        
+      }
+    }.open
+  end
+end
+
+HelloListSingleSelection.new.launch
