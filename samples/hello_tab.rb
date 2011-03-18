bb213dd493f32d05ff726e61dff965014480330c
Andy Maleh <andy@obtiva.com>
2011-03-18 14:40:04 -0600

    Hello tab sample

---
+++ samples/hello_tab.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + "/../lib/glimmer"
+
+class HelloTab
+  include Glimmer
+  def launch
+    shell {
+      text "SWT"
+      tab_folder {
+        tab_item {
+          text "Tab 1"
+          label { 
+            text "Hello World!" 
+          }
+        }
+        tab_item {
+          text "Tab 2"
+          label { 
+            text "Bonjour Univers!" 
+          }
+        }
+      }
+    }.open
+  end
+end
+
+HelloTab.new.launch
