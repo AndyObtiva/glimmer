147462164d22f9bd4ff40f8353492d93ecb8494e
Andy Maleh <andy@obtiva.com>
2011-03-18 14:37:00 -0600

    Simplified hello world sample

--- samples/hello_world.rb
+++ samples/hello_world.rb
@@ -1,21 +1,10 @@
-require "java"
 require File.dirname(__FILE__) + "/../lib/glimmer"
 
-class HelloWorld
-	include_package 'org.eclipse.swt'
-	include_package 'org.eclipse.swt.layout'
-	
-	include Glimmer
+include Glimmer
 
-	def launch
-		@shell = shell {
-			text "SWT"
-      label {
-        text "Hello World!"
-      }
-		}
-    @shell.open
-	end
-end
-
-HelloWorld.new.launch
+shell {
+  text "SWT"
+  label {
+    text "Hello World!"
+  }
+}.open
