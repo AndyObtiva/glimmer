f95938fc3d27b04bf713960202439e3a2da3702f
AndyObtiva <andy@obtiva.com>
2017-07-25 10:56:26 -0400

    Observable model and array now use set in place of array

--- lib/command_handlers/models/observable_array.rb
+++ lib/command_handlers/models/observable_array.rb
@@ -1,9 +1,10 @@
+require 'set'
 module ObservableArray
-  
+
   def add_observer(element_properties, observer)
     property_observer_list << observer
     each do |element|
-      element_properties.each do |property|
+      [element_properties].flatten.each do |property|
         element.extend(ObservableModel) unless element.is_a?(ObservableModel)
         element.add_observer(property, observer)
       end
@@ -11,30 +12,30 @@ module ObservableArray
   end
 
   def property_observer_list
-    @property_observer_list = [] unless @property_observer_list
+    @property_observer_list = Set.new unless @property_observer_list
     @property_observer_list
   end
-  
+
   def notify_observers
     property_observer_list.each {|observer| observer.update}
   end
-  
+
   def self.extend_object(array)
     array.instance_eval("alias original_add <<")
     array.instance_eval <<-end_eval, __FILE__, __LINE__
-      def <<(value) 
-        self.original_add(value) 
-        notify_observers 
+      def <<(value)
+        self.original_add(value)
+        notify_observers
       end
     end_eval
-    
+
     notify_observers_on_invokation(array, "delete", 1)
     notify_observers_on_invokation(array, "delete_at", 1)
     notify_observers_on_invokation(array, "clear")
-    
+
     super
   end
-  
+
   def self.notify_observers_on_invokation(model, method, argument_count=0)
     model.instance_eval "alias original_#{method} #{method}\n"
     arguments = ""
@@ -44,8 +45,8 @@ module ObservableArray
     arguments = arguments[0..-2]
     model.instance_eval <<-end_eval, __FILE__, __LINE__
       def #{method}(#{arguments})
-        self.original_#{method}(#{arguments}) 
-        notify_observers 
+        self.original_#{method}(#{arguments})
+        notify_observers
       end
     end_eval
   end
--- lib/command_handlers/models/observable_model.rb
+++ lib/command_handlers/models/observable_model.rb
@@ -1,5 +1,6 @@
+require 'set'
 module ObservableModel
-  
+
   def add_observer(property_name, observer)
     property_observer_list(property_name) << observer
   end
@@ -8,16 +9,16 @@ module ObservableModel
     @property_observers = Hash.new unless @property_observers
     @property_observers
   end
-  
+
   def property_observer_list(property_name)
-    property_observer_hash[property_name.to_sym] = [] unless property_observer_hash[property_name.to_sym]
+    property_observer_hash[property_name.to_sym] = Set.new unless property_observer_hash[property_name.to_sym]
     property_observer_hash[property_name.to_sym]
   end
-  
+
   def notify_observers(property_name)
     property_observer_list(property_name).each {|observer| observer.update(send(property_name))}
   end
-  
+
   class Updater
     def initialize(property_name, observable_model)
       @property_name = property_name
@@ -27,33 +28,33 @@ module ObservableModel
       @observable_model.notify_observers(@property_name)
     end
   end
-  
+
   def self.extend_object(model)
     super
     model.methods.each do |method|
       self.add_method_observers(model, method)
     end
   end
-  
+
   def self.add_method_observers(model, method)
     setter_method_pattern = /^(\w+=)$/
     if (method.match(setter_method_pattern))
       getter_method = method[0, method.length - 1]
       getter_value = model.send(getter_method)
-      if (getter_value.is_a?(Array) and 
+      if (getter_value.is_a?(Array) and
           !getter_value.is_a?(ObservableArray))
         getter_value.extend(ObservableArray)
         getter_value.add_observer([], Updater.new(getter_method, model))
       end
       model.instance_eval "alias original_#{method} #{method}\n"
       model.instance_eval <<-end_eval, __FILE__, __LINE__
-        def #{method}(value) 
+        def #{method}(value)
           self.original_#{method}(value)
           notify_observers('#{getter_method}')
           if (value.is_a?(Array) and !value.is_a?(ObservableArray))
             value.extend(ObservableArray)
             value.add_observer([], ObservableModel::Updater.new('#{getter_method}', self))
-          end  
+          end
         end
       end_eval
     end
