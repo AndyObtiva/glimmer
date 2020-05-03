bb2a92b10b995ea593fdc280263e872d6fdb7d9e
Andy Maleh <andy.am@gmail.com>
2020-05-03 05:08:45 -0400

    Refactored samples directory

--- samples/video/hello_looped_video_with_black_background.rb
+++
@@ -1,8 +0,0 @@
-include Glimmer
-
-video_file = File.expand_path('../Blackpool_Timelapse.mp4', __FILE__)
-
-shell {
-  minimum_size 1024, 640
-  video(file: video_file, looped: true, background: :black)
-}.open
--- samples/video/hello_video.rb
+++
@@ -1,7 +0,0 @@
-include Glimmer
-
-video_file = File.expand_path('../Clouds_passing_by_CCBY_NatureClip.mp4', __FILE__)
-
-shell {
-  video(file: video_file)
-}.open
